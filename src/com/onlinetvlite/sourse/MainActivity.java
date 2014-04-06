080cc826abb797655ad78fffee6411aa759dbac4
alexeev_alexey <alexeevac@gmail.com>
2014-04-06 02:04:46 +0000

    Classes

---
+++ src/com/onlinetvlite/sourse/MainActivity.java
@@ -0,0 +1,113 @@
+package com.onlinetvlite.sourse;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.ads.AdRequest;
+import com.google.ads.AdView;
+
+public class MainActivity extends Activity{
+
+	private AdView adView;
+	//private InterstitialAd interstitial;
+	private ListView listView;
+	private ArrayAdapter<String> adapter;
+	private String [] channelsNames;
+	private String [] channelsPathes;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		adView = (AdView)findViewById(R.id.adViewMainActivity);
+		adView.loadAd(new AdRequest());
+
+		//interstitial = new InterstitialAd(this, getResources().getString(R.string.adword_page));
+		//interstitial.loadAd(new AdRequest());
+
+		channelsNames = getResources().getStringArray(R.array.channels_names);
+		channelsPathes = getResources().getStringArray(R.array.channels_path);
+		listView = (ListView)findViewById(R.id.list_view_main);
+		adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, channelsNames);
+		listView.setAdapter(adapter);
+		listView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				if(checkNetworkConnection()){
+					Intent intent = new Intent(getApplicationContext(), VideoActivity.class);
+					intent.putExtra("channelPath", channelsPathes[position]);
+					startActivity(intent);
+				}else {
+					Toast.makeText(getApplicationContext(), "Не удалось подключиться к интернету", Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.exit:
+			//interstitial.show();
+			finish();
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public boolean checkNetworkConnection() {
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netInfo = cm.getActiveNetworkInfo();
+		if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void onDestroy() {
+		if (adView != null) {
+			adView.destroy();
+		}
+		super.onDestroy();
+	}
+
+	@Override
+	public void onBackPressed() {
+		super.onBackPressed();
+		//interstitial.show();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+	
+
+}
---
+++ src/com/onlinetvlite/sourse/VideoActivity.java
@@ -0,0 +1,149 @@
+package com.onlinetvlite.sourse;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import com.google.ads.AdRequest;
+import com.google.ads.AdView;
+
+public class VideoActivity extends Activity{
+
+	private Intent intent;
+	private String channelPath;
+	private VideoView videoView;
+	private ProgressDialog progressDialog;
+	private ListView listView;
+	private ArrayAdapter<String> adapter;
+	private String [] channelsNames;
+	private String [] channelsPathes;
+	private AdView adView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		setContentView(R.layout.activity_video);
+		adView = (AdView)findViewById(R.id.adViewVideoActivity);
+		adView.loadAd(new AdRequest());
+
+		//linearLayout = (LinearLayout)findViewById(R.id.linearLayout);
+		
+		channelsNames = getResources().getStringArray(R.array.channels_names);
+		channelsPathes = getResources().getStringArray(R.array.channels_path);
+		adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.my_item, channelsNames);
+		intent = getIntent();
+		channelPath = intent.getStringExtra("channelPath");
+		listView = (ListView)findViewById(R.id.list_view_video);
+		listView.setVisibility(View.GONE);
+		adView.setVisibility(View.GONE);
+		listView.setAdapter(adapter);
+		videoView = (VideoView)findViewById(R.id.video_view);
+		videoView.setOnPreparedListener(new OnPreparedListener() {
+
+			@Override
+			public void onPrepared(MediaPlayer mp) {
+				if(progressDialog != null && progressDialog.isShowing()){
+					progressDialog.dismiss();
+					progressDialog = null;
+				}
+				videoView.start();
+			}
+		});
+		videoView.setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				if(listView.getVisibility() == View.VISIBLE) {
+					listView.setVisibility(View.GONE);
+					adView.setVisibility(View.GONE);
+				}
+				else {
+					listView.setVisibility(View.VISIBLE);
+					adView.setVisibility(View.VISIBLE);
+				}
+				return false;
+			}
+		});
+		listView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+				playTV(channelsPathes[position]);
+			}
+		});
+		playTV(channelPath);
+	}
+
+	public void playTV(String channelPath) {
+		if(progressDialog != null) progressDialog.dismiss();
+		if(checkNetworkConnection()) {			
+			Uri video = Uri.parse(channelPath);
+			videoView.setVideoURI(video);
+			showProgressDoalog();	
+		} else {
+			Toast.makeText(getApplicationContext(), "Не удалось подключиться к интернету", Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	public boolean checkNetworkConnection() {
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netInfo = cm.getActiveNetworkInfo();
+		if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+			return true;
+		}
+		return false;
+	}
+
+	private void showProgressDoalog() {
+		progressDialog = new ProgressDialog(this);
+		progressDialog.setTitle("Подключение к серверу" );
+		progressDialog.setMessage("Подождите пожалуйста");
+		progressDialog.setCancelable(true);
+		progressDialog.setCanceledOnTouchOutside(false);
+		progressDialog.show();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (adView != null) {
+			adView.destroy();
+		}
+		if(progressDialog != null) progressDialog.dismiss();
+		videoView.stopPlayback();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		videoView.suspend();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		videoView.resume();
+	}	
+}
